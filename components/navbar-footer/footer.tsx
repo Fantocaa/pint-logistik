2b87090463f41ac0184a7a269938512a4e0f4e78
Fantoca.Tako <fantoca@tako.co.id>
2024-05-15 09:11:32 +0700

    Update footer.tsx

--- components/navbar-footer/footer.tsx
+++ components/navbar-footer/footer.tsx
@@ -29,7 +29,7 @@ export default function Footer() {
   return (
     <>
       <footer className="bg-darkpint">
-        <div className="relative mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8 lg:pt-24">
+        <div className="relative mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8 lg:pt-8">
           <div className="absolute end-4 top-4 sm:end-6 sm:top-6 lg:end-8 lg:top-8">
             <button
               onClick={handleBackToTopClick}
@@ -63,10 +63,56 @@ export default function Footer() {
                 />
               </div>
 
-              <p className="mx-auto max-w-md text-center leading-relaxed text-gray-100 lg:text-left my-8">
-                Komplek Ruko Semut Square Blok D no.11 Jalan Semut kali no.48-50
-                Kec. Pabean Cantikan, Surabaya
-              </p>
+              <div className="flex items-center gap-4 mt-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="lucide lucide-building text-white"
+                >
+                  <rect width="16" height="20" x="4" y="2" rx="2" ry="2" />
+                  <path d="M9 22v-4h6v4" />
+                  <path d="M8 6h.01" />
+                  <path d="M16 6h.01" />
+                  <path d="M12 6h.01" />
+                  <path d="M12 10h.01" />
+                  <path d="M12 14h.01" />
+                  <path d="M16 10h.01" />
+                  <path d="M16 14h.01" />
+                  <path d="M8 10h.01" />
+                  <path d="M8 14h.01" />
+                </svg>
+                <p className="mx-auto max-w-md text-center leading-relaxed text-gray-100 lg:text-left my-4">
+                  Komplek Ruko Semut Square Blok D no.11 Jalan Semut kali
+                  no.48-50 Kec. Pabean Cantikan, Surabaya
+                </p>
+              </div>
+
+              <div className="flex items-center gap-4 mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="lucide lucide-phone text-white"
+                >
+                  <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
+                </svg>
+                <p className=" text-center leading-relaxed text-gray-100 lg:text-left my-4">
+                  031 - 99096665
+                </p>
+              </div>
               <Link href="https://wa.link/qnedf4" target="_blank">
                 <Button variant="secondary">
                   <Image
